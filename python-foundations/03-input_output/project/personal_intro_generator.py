2e324dedd6b99b31c27483f0b3404102343a3e28
Avi Prasad <avi.prasad.dev@gmail.com>
2026-06-04 22:14:30 +0530

    Create project solution and completed topic 03 input and output

---
+++ python-foundations/03-input_output/project/personal_intro_generator.py
@@ -0,0 +1,9 @@
+name: str = str(input("Enter Your name: "))
+age: int = int(input("Enter your Age: "))
+city: str = str(input("Enter your city: "))
+current_job: str = str(input("Enter your current job: "))
+
+print(f"Hello {name}")
+print(f"You are {age} years old")
+print(f"You live in {city}")
+print(f"I currently work as a {current_job}")
