429e42ce800d90b17884788d882f0d7e76b84559
Avi Prasad <avi.prasad.dev@gmail.com>
2026-06-05 13:17:49 +0530

    Created solution of challenge 04

---
+++ python-foundations/04-operators/solution_04.py
@@ -0,0 +1,13 @@
+first_number: int = int(input("Enter first number: "))
+second_number: int = int(input("Enter second number: "))
+
+add: int = first_number + second_number
+sub: int = first_number - second_number
+mul: int = first_number * second_number
+div: float = first_number / second_number
+print("")
+
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")
